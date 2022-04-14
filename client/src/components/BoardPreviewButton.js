c2c9812a230e4371bf44f7e48858c94a941499e8
RyanMcPherson7 <ryan7mcpherson@gmail.com>
2022-04-13 23:59:46 -0400

    added styling for board preview buttons

--- client/src/components/BoardPreviewButton.js
+++ client/src/components/BoardPreviewButton.js
@@ -1,10 +1,11 @@
+import { BsFillKanbanFill } from 'react-icons/bs';
+
 const BoardPreviewButton = ({ board }) => {
   return (
-    <div className='dashboard-button'>
+    <div className='dashboard-button board-preview-button'>
+      <BsFillKanbanFill style={{width: '2rem', height: '2rem'}}/>
       <h3>{board.name}</h3>
-      <p>
-        by: <span className='orange'>{board.ownerName}</span>
-      </p>
+      <p>{board.ownerName}</p>
     </div>
   );
 };
