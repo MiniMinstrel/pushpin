da8ffa7aa2e30106716759b27af76899f2e35c62
Mariano Patino-Paul <marianoppu@hotmail.com>
2022-04-16 13:51:11 -0400

    warnings/alerts added before post/board deletion

--- client/src/components/DeletePostButton.js
+++ client/src/components/DeletePostButton.js
@@ -1,9 +1,10 @@
 import { FaTrash } from 'react-icons/fa';
 import { deletePost } from '../firebase/firebase-db';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
 
 const DeletePostButton = ({ boardId, postId }) => {
-
 
 const navigate = useNavigate();
 
@@ -12,15 +13,33 @@ const navigate = useNavigate();
     navigate('/dashboard');
     };
 
-  return (
+  const [showModal, setShowModal] = useState(false);
+  const handleShow = () => setShowModal(true);
+  const handleClose = () => setShowModal(false);
 
-    <div
-      className='create-delete-button'
-      id='delete-post-button'
-      onClick={handleSubmit}
-    >
-      <FaTrash /> Delete
-    </div>
+  return (
+    <>
+      <div
+        className='create-delete-button'
+        id='delete-post-button'
+        onClick={handleShow}
+      >
+        <FaTrash /> Delete
+      </div>
+
+      <Modal className='modal' show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>Are you sure you want to delete this post?</Modal.Header>
+        <Modal.Body>
+          This action can't be undone
+        </Modal.Body>
+        <Modal.Footer>
+          <button onClick={handleSubmit}>
+            <FaTrash />
+            Delete
+          </button>
+        </Modal.Footer>
+      </Modal>
+    </>
 
   );
 };
